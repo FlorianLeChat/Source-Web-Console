c4420fa42af51b79e68f3359ba0bab485af46c12
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-03-23 20:16:30 +0100

    Added JavaScript changes to the statistics page

--- scripts/statistics.js
+++ scripts/statistics.js
@@ -6,93 +6,102 @@ const player_chart = $( "#player_count" );
 
 new Chart( player_chart,
 	{
-		type: 'bar',
+		type: "line",
 		data: {
-			labels: [ 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange' ],
-			datasets: [ {
-				label: '# of Votes',
-				data: [ 12, 19, 3, 5, 2, 3 ],
-				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
-				],
-				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
-				],
-				borderWidth: 1
-			} ]
+			datasets: [
+				{
+					data: [ 12, 4, 1, 0, 0, 0, 1, 3, 5, 1, 2, 4, 5, 6, 4, 15, 16, 18, 23, 45, 24, 26, 45, 34, 24 ],
+					fill: true,
+					borderColor: "#f59a23",
+					borderWidth: 3,
+					hoverBorderColor: "#797979",
+					hoverBackgroundColor: "#f2f2f2"
+				}
+			]
 		},
 		options: {
+			responsive: true,
+			interaction: {
+				mode: "nearest",
+				intersect: false,
+			},
+			plugins: {
+				legend: {
+					display: false,
+				}
+			},
 			scales: {
+				x: {
+					labels: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 21, 23 ],
+					grid: {
+						color: "#797979"
+					},
+					ticks: {
+						color: "#f59a23",
+						font: {
+							size: 16
+						},
+						stepSize: 1,
+						beginAtZero: true
+					},
+					title: {
+						display: true,
+						text: "Heure UTC",
+						color: "#f59a23",
+						font: {
+							size: 20,
+							family: "Roboto",
+							weight: "bold",
+							lineHeight: 1.2,
+						},
+						padding: { top: 15, left: 0, right: 0, bottom: 0 }
+					}
+				},
 				y: {
-					beginAtZero: true
+					max: 128,
+					labels: [ 0, 128 ],
+					grid: {
+						color: "#797979"
+					},
+					ticks: {
+						color: "#f59a23",
+						font: {
+							size: 16,
+						},
+						stepSize: 2,
+						beginAtZero: true
+					},
+					title: {
+						display: true,
+						text: "Nombre de joueurs",
+						color: "#f59a23",
+						font: {
+							size: 20,
+							family: "Roboto",
+							weight: "bold",
+							lineHeight: 1.2
+						},
+						padding: { top: 0, left: 0, right: 0, bottom: 10 }
+					}
 				}
 			}
 		}
 	} );
 
 //
+// Permet de créer un graphique pour illustrer les statistiques
+//	d'utilisation du serveur en fonction du temps.
 //
-//
-const cpu_usage = $( "#cpu_usage" );
+const server_usage = $( "#server_usage" );
 
-new Chart( cpu_usage,
+new Chart( server_usage,
+
 	{
 		type: 'bar',
 		data: {
 			labels: [ 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange' ],
 			datasets: [ {
-				label: '# of Votes',
-				data: [ 12, 19, 3, 5, 2, 3 ],
-				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
-				],
-				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
-				],
-				borderWidth: 1
-			} ]
-		},
-		options: {
-			scales: {
-				y: {
-					beginAtZero: true
-				}
-			}
-		}
-	} );
-
-//
-//
-//
-const memory_usage = $( "#memory_usage" );
-
-new Chart( memory_usage,
-	{
-		type: 'bar',
-		data: {
-			labels: [ 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange' ],
-			datasets: [ {
-				label: '# of Votes',
+				label: 'Nombre de joueurs',
 				data: [ 12, 19, 3, 5, 2, 3 ],
 				backgroundColor: [
 					'rgba(255, 99, 132, 0.2)',
