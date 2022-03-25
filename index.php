498f2b44d6e5af8967f99b6645b6195be8eaa063
FlorianLeChat <floriantrayon942@gmail.com>
2022-03-25 16:35:32 +0100

    Improved the TWIG generation mecanism

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
 	// Variables fixes.
 	$page = $_GET["target"] ?? "index";
 	$title = "Administration";
-	$subtitle = "";
 	$language = "FR";
-
 	$titles = [
 		"dashboard" => "Tableau de bord",
 		"statistics" => "Statistiques",
@@ -17,47 +15,22 @@
 		"tasks" => "Tâches planifiées"
 	];
 
-	// En-tête du document.
-	$head_html = $twig->render("1_head.twig",
+	$html = $twig->render("$page.twig",
 	[
-		"url" => $_SERVER["SERVER_NAME"],
-		"file" => $page,
-		"language" => $language,
-		"keywords" => "word1, word2, word3, ...",
-		"description" => "Description succinte du site..."
+		// Page entière.
+		"page_file" => $page,
+		"page_title" => "Source Web Console",
+		"page_language" => $language,
+
+		// En-tête du document.
+		"head_url" => $_SERVER["SERVER_NAME"],
+		"head_keywords" => "word1, word2, word3, ...",
+		"head_description" => "Description succinte du site...",
+
+		// En-tête de la page.
+		"header_title" => $title,
+		"header_subtitle" => $titles[$page] ?? $title
 	]);
 
-	// En-tête de la page.
-	$header_html = $twig->render("2_header.twig",
-	[
-		"title" => $title,
-		"subtitle" => $titles[$page] ?? $title
-	]);
-
-	// Barre de navigation.
-	$navigation_html = $twig->render("3_navigation.twig");
-
-	// Formulaires.
-	$signup_html = $twig->render("4_signup.twig");
-	$signin_html = $twig->render("5_signin.twig");
-	$contact_html = $twig->render("6_contact.twig");
-
-	// Pied-de-page.
-	$footer_html = $twig->render("7_footer.twig");
-
-	// Page entière.
-	$main_html = $twig->render("$page.twig",
-	[
-		"title" => "Source Web Console",
-		"language" => $language,
-		"head_html" => $head_html,
-		"header_html" => $header_html,
-		"navigation_html" => $navigation_html,
-		"signup_html" => $signup_html,
-		"signin_html" => $signin_html,
-		"contact_html" => $contact_html,
-		"footer_html" => $footer_html
-	]);
-
-	echo($main_html);
+	echo($html);
 ?>
