9523b9859288ce2e8f46cd94ba35b7f674fba57c
Florian Trayon <git@florian-trayon.fr>
2022-03-28 17:02:42 +0200

    Added a working version of the index carousel

--- scripts/index.js
+++ scripts/index.js
@@ -112,18 +112,130 @@ redirections.last().click( function ()
 // Permet de contrôler le mécanisme de présentation des fonctionnalités
 //	principales du site.
 //
-const informations = $( "#informations ul" );
-const images = informations.first();
-const texts = informations.last();
+const informations = $( "#informations" );		// Conteneur général des informations.
+const elements = informations.find( "ul" ); 	// Les deux listes : paragraphes et images.
+const images = elements.first().children(); 	// Liste des images.
+const texts = elements.last().children();		// Liste des paragraphes.
+const length = images.length - 1;				// Longueur de la liste des éléments.
 
-let indice = 0;
-
-for ( const image of images.children() )
+// Permet d'afficher les éléments initiaux de présentation.
+function displayInitialElements()
 {
-	console.log( $( image ).children().attr( "src" ) );
+	const header = informations.find( "article *" );	// Récupération des éléments de présentation.
+	const indice = texts.filter( ":visible" ).index();	// Récupération de la position de l'élément visible.
+	const show = indice == 0 ? true : false;			// Détermination de son affichage ou non.
+
+	// Apparition/disparition des éléments de présentation.
+	header.slice( 0, 2 ).css( "display", show ? "revert" : "none" );
+
+	// Apparition/disparition du bouton pour revenir à un élément précédent.
+	header.eq( 2 ).css( "display", show ? "none" : "revert" );
 }
 
-for ( const text of texts.children() )
+// Permet d'exécuter le mécanisme de défilement précédent/suivant.
+function updateInformation( forward )
 {
-	console.log( $( text ).text() );
+	// Défilement des images.
+	for ( const indice in images )
+	{
+		const element = $( images[ indice ] );
+
+		// On vérifie si l'image est actuellement visible.
+		if ( element.is( ":visible" ) )
+		{
+			// Dans ce cas, on cache l'image actuelle.
+			element.hide();
+
+			// On vérifie ensuite si l'utilisateur demander d'avancer
+			//	ou de reculer dans les positions des images.
+			if ( forward )
+			{
+				// Pour avancer, on vérifie si on atteint pas le dépassement
+				//	du nombre d'images disponibles.
+				if ( indice >= length )
+				{
+					// Dans ce cas, on affiche la première image de la liste.
+					images.first().show();
+				}
+				else
+				{
+					// Dans le cas contraire, on affiche la suivante.
+					element.next().show();
+				}
+			}
+			else
+			{
+				// En cas de reculement, on vérifie la position actuelle
+				//	dans la liste.
+				if ( indice == 0 )
+				{
+					// Si on atteint le début de la liste, on affiche la dernière
+					//	image disponible.
+					images.last().show();
+				}
+				else
+				{
+					// Dans le cas contraire, on affiche la précédente.
+					element.prev().show();
+				}
+			}
+
+			break;
+		}
+	}
+
+	// Défilement des paragraphes.
+	for ( const indice in texts )
+	{
+		const element = $( texts[ indice ] );
+
+		// Vérification de la visilité de l'élement.
+		if ( element.is( ":visible" ) )
+		{
+			element.hide();
+
+			// Mécanisme de précédent/suivant.
+			if ( forward )
+			{
+				// Texte suivant.
+				if ( indice >= length )
+				{
+					texts.first().show();
+				}
+				else
+				{
+					element.next().show();
+				}
+			}
+			else
+			{
+				// Texte précédent.
+				if ( indice == 0 )
+				{
+					texts.last().show();
+				}
+				else
+				{
+					element.prev().show();
+				}
+			}
+
+			// Mise à jour des éléments de présentation.
+			displayInitialElements();
+
+			break;
+		}
+	}
 }
+
+informations.find( "button" ).first().click( function ()
+{
+	// Bouton pour voir l'information précédente.
+	updateInformation( false );
+} );
+
+informations.find( "button" ).last().click( function ()
+{
+	// Bouton pour voir l'information suivante.
+	updateInformation( true );
+} );
