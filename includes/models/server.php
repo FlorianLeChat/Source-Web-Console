8daff859f1ceb5f0fc81edbd629564d6b9ab19b7
Florian Trayon <git@florian-trayon.fr>
2022-04-15 17:21:52 +0200

    Added automatic encryption of the RCON password

--- includes/models/server.php
+++ includes/models/server.php
@@ -10,6 +10,85 @@
 
 	final class Server extends Main
 	{
+		// Connecteur Source RCON Query.
+		public SourceQuery $query;
+
+		// Méthode de chiffrement pour le mot de passe administrateur.
+		private const ENCRYPTION_METHOD = "AES-256-CTR";
+
+		//
+		// Permet d'initialiser certains mécanismes lors de l'instanciation
+		//	de la classe actuelle.
+		//
+		public function __construct()
+		{
+			// Exécution du constructeur parent.
+			parent::__construct();
+
+			// Initialisation de la connexion administrateur à l'instance.
+			$this->query = new SourceQuery();
+		}
+
+		//
+		// Permet d'établir une connexion avec une instance distante.
+		//
+		public function connectInstance(string $address, int $port, string $password = "")
+		{
+			// On établit la connexion avec les informations renseignées.
+			$this->query->Connect($address, $port, 1, SourceQuery::SOURCE);
+
+			// On vérifie alors si le mot de passe administrateur est indiqué.
+			if (!empty($password))
+			{
+				// On déchiffre enfin le mot de passe avant de le définir.
+				$this->query->SetRconPassword($this->password_decrypt($password));
+			}
+		}
+
+		//
+		// Permet de chiffrer une chaîne de caractères en utilisant les fonctions
+		//	de la bibliothèque de OpenSSL.
+		// 	Source : https://stackoverflow.com/a/60283328
+		//
+		private function password_encrypt(string $password): string
+		{
+			// On chiffre d'abord la phrase unique de chiffrement.
+			$key = hash("sha256", $this->getConfig("openssl_phrase"));
+
+			// On chiffre ensuite le vecteur d'initialisation.
+			$iv = substr(hash("sha256", $this->getConfig("openssl_iv")), 0, 16);
+
+			// On utilise après la fonction de OpenSSL pour chiffrer
+			//	à deux reprises le mot de passe.
+			$password = openssl_encrypt($password, self::ENCRYPTION_METHOD, $key, 0, $iv);
+			$password = base64_encode($password);
+
+			// On retourne enfin le mot de passe chiffré.
+			return $password;
+		}
+
+		//
+		// Permet de dechiffrer une chaîne de caractères en utilisant les fonctions
+		//	de la bibliothèque de OpenSSL.
+		// 	Source : voir fonction précédente.
+		//
+		private function password_decrypt(string $password): bool
+		{
+			// On chiffre d'abord la phrase unique de chiffrement.
+			$key = hash("sha256", $this->getConfig("openssl_phrase"));
+
+			// On chiffre ensuite le vecteur d'initialisation.
+			$iv = substr(hash("sha256", $this->getConfig("openssl_iv")), 0, 16);
+
+			// On utilise après la fonction de OpenSSL pour déchiffrer
+			//	complétement le mot de passe.
+			$password = base64_decode($password);
+			$password = openssl_decrypt($password, self::ENCRYPTION_METHOD, $key, 0, $iv);
+
+			// On retourne enfin le mot de passe déchiffré.
+			return $password;
+		}
+
 		//
 		// Permet d'enregistrer une nouvelle instance dans la base de données.
 		//
