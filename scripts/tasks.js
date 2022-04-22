a26ec3b83406f59f267265e4a080bd29b9c27b60
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-04-22 18:30:52 +0200

    Added the back-end of the tasks page

---
+++ scripts/tasks.js
@@ -0,0 +1,83 @@
+//
+// Permet de gérer les demandes de suppression de tâches planifiées
+//	dans la base de données.
+//
+$( "table tr:not([class = finished])" ).click( function ()
+{
+	// On vérifie si l'utilisateur demande a supprimer la tâche.
+	if ( confirm( edit_remove ) )
+	{
+		// On réalise ensuite la requête AJAX.
+		$.post( "includes/controllers/server_tasks.php", {
+
+			// Identifiant unique du serveur sélectionné.
+			target_server: $( this ).find( "em" ).attr( "data-server" ),
+
+			// Identifiant unique de la tâche sélectionnée.
+			target_task: $( this ).attr( "data-task" )
+
+		} )
+			.done( function ( data, _status, _self )
+			{
+				// On affiche la notification de confirmation.
+				addQueuedNotification( data, 3 );
+
+				// On recharge enfin la page quelques instants après.
+				//	Note : cela pourrait être amélioré en construisant directement
+				//		le HTML à la réception finale du message.
+				setTimeout( function ()
+				{
+					location.reload();
+				}, 3000 );
+			} )
+			.fail( function ( self, _status, error )
+			{
+				// Dans le cas contraire, on affiche une notification
+				//	d'échec avec les informations à notre disposition.
+				addQueuedNotification( server_fatal_error.replace( "$1", getStatusText( error, self.status ) ), 1 );
+			} );
+	}
+} );
+
+//
+// Permet d'envoyer les demandes de création de tâches planifiées
+//	vers la base de données.
+//
+$( "form input[type = submit]" ).click( function ( event )
+{
+	// On cesse d'abord le comportement par défaut.
+	event.preventDefault();
+
+	// On réalise ensuite la requête AJAX.
+	$.post( "includes/controllers/server_tasks.php", {
+
+		// Identifiant unique du serveur sélectionné.
+		target_server: $( "select[name = server] option:checked" ).attr( "data-server" ),
+
+		// Date de déclenchement de l'action (format horodatage).
+		trigger_date: $( "input[name = date]" ).val(),
+
+		// Nom de l'action qui doit être réalisé.
+		trigger_action: $( "select[name = action]" ).val()
+
+	} )
+		.done( function ( data, _status, _self )
+		{
+			// On affiche la notification de confirmation.
+			addQueuedNotification( data, 3 );
+
+			// On recharge enfin la page quelques instants après.
+			//	Note : cela pourrait être amélioré en construisant directement
+			//		le HTML à la réception finale du message.
+			setTimeout( function ()
+			{
+				location.reload();
+			}, 3000 );
+		} )
+		.fail( function ( self, _status, error )
+		{
+			// Dans le cas contraire, on affiche une notification
+			//	d'échec avec les informations à notre disposition.
+			addQueuedNotification( server_fatal_error.replace( "$1", getStatusText( error, self.status ) ), 1 );
+		} );
+} );
