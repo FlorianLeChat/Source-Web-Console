c424d57efaa56486cd6767ec4b23d24d474034c6
Florian Trayon <git@florian-trayon.fr>
2022-04-26 16:18:26 +0200

    Added the final version of the configuration page

--- includes/controllers/page_configuration.php
+++ includes/controllers/page_configuration.php
@@ -3,15 +3,15 @@
 	// Contrôleur de gestion de la page de la configuration.
 	//
 
-	// On récupère tout d'abord l'idenfiant unique de l'utilisateur
-	$user_id = $_SESSION["user_id"] ?? 0;
+	// On récupère tout d'abord l'idenfiant unique du serveur.
+	$server_id = $_SESSION["server_id"] ?? 0;
 
 	// On inclut enfin les paramètres du moteur TWIG pour la création
 	//	finale de la page.
 	$parameters = [
 
 		// Identifiants du serveur de stockage.
-		"configuration_credentials" => $server->getExternalStorage($user_id, $server_id)
+		"configuration_credentials" => $server->getExternalStorage($server_id)
 
 	];
 ?>
