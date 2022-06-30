fa087ff464e2de76a1e6834f1775009eb093b1e9
Florian Trayon <git@florian-trayon.fr>
2022-06-30 19:54:48 +0200

    Fixed the header of contact emails

--- includes/models/form.php
+++ includes/models/form.php
@@ -80,14 +80,11 @@
 		// Permet de « rendre propre » des chaînes de caractères pour
 		//	détecter les entrées invalides ou malveillantes.
 		//
-		public function serializeInput(string $field, string $input, bool $encode = true): string|false
+		public function serializeInput(string $field, string $input): string|false
 		{
-			// On encode d'abord caractères spéciaux en balises
-			//	HTML lisibles si demandé explicitement.
-			if ($encode)
-			{
-				$input = htmlentities($input, ENT_QUOTES);
-			}
+			// On encode d'abord les caractères spéciaux en balises
+			//	HTML lisibles.
+			$input = htmlentities($input, ENT_QUOTES);
 
 			// On supprime les espaces en trop en début et à la fin de
 			//	la chaîne de caractères.
