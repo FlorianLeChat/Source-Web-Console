251684f259a5ed71660cec20f4957e002d1146b2
Florian Trayon <git@florian-trayon.fr>
2023-03-29 22:35:21 +0200

    Added home page controller (index)

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,16 +1,19 @@
 <?php
 
+//
+// Contrôleur de la page d'accueil.
+//
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
-    #[Route("/", name: "app_index")]
-    public function index(): Response
-    {
-        return $this->render("index.html.twig");
-    }
+	#[Route("/")]
+	public function index(): Response
+	{
+		return $this->render("index.html.twig");
+	}
 }
