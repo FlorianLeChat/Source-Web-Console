d653fdca63bf7ecfaad39deef92b8967647505bb
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-03-31 10:01:47 +0200

    Added SASS file compilation through Webpack Encore

--- config/preload.php
+++ config/preload.php
@@ -2,5 +2,5 @@
 
 if (file_exists(dirname(__DIR__) . "/var/cache/prod/App_KernelProdContainer.preload.php"))
 {
-    require dirname(__DIR__) . "/var/cache/prod/App_KernelProdContainer.preload.php";
+	require dirname(__DIR__) . "/var/cache/prod/App_KernelProdContainer.preload.php";
 }
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const Encore = require( "@symfony/webpack-encore" );
+
+if ( !Encore.isRuntimeEnvironmentConfigured() )
+{
+	Encore.configureRuntimeEnvironment( process.env.NODE_ENV || "dev" );
+}
+
+Encore
+	.setOutputPath( "public/build/" )
+	.setPublicPath( "/build" )
+	.addEntry( "app", "./assets/app.js" )
+	.enableStimulusBridge( "./assets/controllers.json" )
+	.splitEntryChunks()
+	.enableSingleRuntimeChunk()
+	.cleanupOutputBeforeBuild()
+	.enableBuildNotifications()
+	.enableSourceMaps( !Encore.isProduction() )
+	.enableVersioning( Encore.isProduction() )
+	.configureBabelPresetEnv( ( config ) =>
+	{
+		config.useBuiltIns = "usage";
+		config.corejs = "3.23";
+	} )
+	.enableSassLoader()
+	.enableIntegrityHashes( Encore.isProduction() );
+
+module.exports = Encore.getWebpackConfig();
