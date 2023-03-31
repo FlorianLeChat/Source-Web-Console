1e6ae4345d46ebe5b45e65a2f17af5a3b4370fe2
Florian Trayon <git@florian-trayon.fr>
2023-03-31 12:05:19 +0200

    Moved static files to Webpack folder

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,43 @@
-// Dépendances JavaScript.
-import "jquery";
-import "chart.js";
-import "vanilla-lazyload";
-import "vanilla-cookieconsent";
-import "chartjs-adapter-date-fns";
+// Fichiers principaux (SASS et JS).
+import "./styles/_global.scss";
+import "./scripts/main.js";
 
+// Fichiers de styles.
+import "./styles/desktop/actions.scss";
+import "./styles/desktop/admin.scss";
+import "./styles/desktop/configuration.scss";
+import "./styles/desktop/console.scss";
+import "./styles/desktop/dashboard.scss";
+import "./styles/desktop/help.scss";
+import "./styles/desktop/index.scss";
+import "./styles/desktop/legal.scss";
+import "./styles/desktop/statistics.scss";
+import "./styles/desktop/tasks.scss";
+import "./styles/desktop/user.scss";
+
+import "./styles/phone/actions.scss";
+import "./styles/phone/admin.scss";
+import "./styles/phone/configuration.scss";
+import "./styles/phone/console.scss";
+import "./styles/phone/dashboard.scss";
+import "./styles/phone/help.scss";
+import "./styles/phone/index.scss";
+import "./styles/phone/legal.scss";
+import "./styles/phone/statistics.scss";
+import "./styles/phone/tasks.scss";
+import "./styles/phone/user.scss";
+
+import "./styles/tablet/actions.scss";
+import "./styles/tablet/admin.scss";
+import "./styles/tablet/configuration.scss";
+import "./styles/tablet/console.scss";
+import "./styles/tablet/dashboard.scss";
+import "./styles/tablet/help.scss";
+import "./styles/tablet/index.scss";
+import "./styles/tablet/legal.scss";
+import "./styles/tablet/statistics.scss";
+import "./styles/tablet/tasks.scss";
+import "./styles/tablet/user.scss";
+
+// Démarrage des contrôleurs Stimulus.
 import "./bootstrap";
