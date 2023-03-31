4866cecd0058a6fcb25d8cb208c421fda04771fa
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-03-31 12:05:19 +0200

    Moved static files to Webpack folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,19 @@ if ( !Encore.isRuntimeEnvironmentConfigured() )
 Encore
 	.setOutputPath( "public/build/" )
 	.setPublicPath( "/build" )
-	.addEntry( "app", "./assets/app.js" )
+	.addEntry( "global", "./assets/app.js" )
+	.addEntry( "actions", "./assets/scripts/actions.js" )
+	.addEntry( "configuration", "./assets/scripts/configuration.js" )
+	.addEntry( "console", "./assets/scripts/console.js" )
+	.addEntry( "dashboard", "./assets/scripts/dashboard.js" )
+	.addEntry( "help", "./assets/scripts/help.js" )
+	.addEntry( "index", "./assets/scripts/index.js" )
+	.addEntry( "statistics", "./assets/scripts/statistics.js" )
+	.addEntry( "tasks", "./assets/scripts/tasks.js" )
+	.addEntry( "user", "./assets/scripts/user.js" )
 	.enableStimulusBridge( "./assets/controllers.json" )
 	.splitEntryChunks()
+	.autoProvidejQuery()
 	.enableSingleRuntimeChunk()
 	.cleanupOutputBeforeBuild()
 	.enableBuildNotifications()
@@ -20,6 +30,16 @@ Encore
 	{
 		config.useBuiltIns = "usage";
 		config.corejs = "3.23";
+	} )
+	.copyFiles( [
+		{ from: "./assets/images", to: "images/[path][name].[hash:8].[ext]" },
+		{ from: "./assets/videos", to: "videos/[path][name].[hash:8].[ext]" }
+	] )
+	.configureImageRule( {
+		type: "asset",
+	} )
+	.configureFontRule( {
+		type: "asset",
 	} )
 	.enableSassLoader()
 	.enableIntegrityHashes( Encore.isProduction() );
