7c8ed0c57776083ae97aefabc65ff4f63b4dcbad
Florian Trayon <git@florian-trayon.fr>
2023-03-31 11:14:18 +0200

    Improved CSS/JS dependencies importation through npm

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,8 @@
-import "./styles/app.css";
+// Dépendances JavaScript.
+import "jquery";
+import "chart.js";
+import "vanilla-lazyload";
+import "vanilla-cookieconsent";
+import "chartjs-adapter-date-fns";
+
 import "./bootstrap";
