fda9f5a157a2459afe1dc6e51d3994f2f7fa6d03
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-01 09:48:06 +0200

    Fixed route to legal notice page

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -1,16 +1,19 @@
 <?php
 
+//
+// Contrôleur de la page des mentions légales.
+//
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LegalController extends AbstractController
 {
-    #[Route("/legal", name: "app_legal")]
-    public function index(): Response
-    {
-        return $this->render("legal.html.twig");
-    }
+	#[Route("/legal")]
+	public function index(): Response
+	{
+		return $this->render("legal.html.twig");
+	}
 }
