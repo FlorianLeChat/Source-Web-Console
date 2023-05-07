7e6f93afc3d104bf2532fc732eafef291892c56a
Florian Trayon <git@florian-trayon.fr>
2023-05-07 15:14:21 +0200

    Added default controller for action and command page

--- src/Controller/ActionsController.php
+++ src/Controller/ActionsController.php
@@ -1,16 +1,23 @@
 <?php
 
+//
+// Contrôleur de la page des actions et des commandes.
+//
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ActionsController extends AbstractController
 {
-	#[Route("/actions", name: "app_actions")]
+	#[Route("/actions")]
 	public function index(): Response
 	{
+		// On vérifie d'abord que l'utilisateur est bien authentifié.
+		$this->denyAccessUnlessGranted("IS_AUTHENTICATED");
+
+		// Si c'est le cas, on le redirige enfin vers la page des actions.
 		return $this->render("actions.html.twig");
 	}
 }
