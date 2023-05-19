7d2a72fee65db869d360e08468a5fb10b4cd03f9
Florian Trayon <git@florian-trayon.fr>
2023-05-19 11:45:16 +0200

    Replaced image and video lazy loading with browser one

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -453,18 +453,6 @@ function sendAnalytics()
 }
 
 //
-// Permet d'implémenter le chargement différé de certaines ressources (images, vidéos, ...).
-//  Source : https://github.com/verlok/vanilla-lazyload#-getting-started---script
-//
-import LazyLoad from "vanilla-lazyload";
-
-const resources = $( "img, video, [data-bg]" );
-resources.addClass( "lazy" );
-
-const lazyLoad = new LazyLoad();
-lazyLoad.update();
-
-//
 // Permet de demander le consentement de l'utilisateur pour utiliser le mécanisme des cookies.
 //  Note : ne s'applique pas à la page des mentions légales.
 //  Source : https://github.com/orestbida/cookieconsent#all-configuration-options
