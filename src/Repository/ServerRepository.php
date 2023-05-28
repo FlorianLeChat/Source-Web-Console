89ba8d635d416e15e0f9f65ab1a8c2b0d485e788
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-05-28 16:29:36 +0200

    Removed unused methods created by default with Doctrine

--- src/Repository/ServerRepository.php
+++ src/Repository/ServerRepository.php
@@ -43,29 +43,4 @@ class ServerRepository extends ServiceEntityRepository
 			$this->getEntityManager()->flush();
 		}
 	}
-
-//    /**
-//     * @return Server[] Returns an array of Server objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Server
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
