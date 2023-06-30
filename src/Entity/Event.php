47ee269f56bac7b0f78d7998a3b607afeeecfc3b
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-06-30 21:22:16 +0200

    Added Doctrine entity for logged events

---
+++ src/Entity/Event.php
@@ -0,0 +1,70 @@
+<?php
+
+//
+// Entité pour les événements journalisés.
+//
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+	#[ORM\Id]
+	#[ORM\GeneratedValue]
+	#[ORM\Column]
+	private ?int $id = null;
+
+	#[ORM\ManyToOne(inversedBy: "events")]
+	#[ORM\JoinColumn(nullable: false)]
+	private ?Server $server = null;
+
+	#[ORM\Column(type: Types::DATETIME_MUTABLE)]
+	private ?\DateTimeInterface $date = null;
+
+	#[ORM\Column(length: 10)]
+	private ?string $action = null;
+
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
+
+	public function getServer(): ?Server
+	{
+		return $this->server;
+	}
+
+	public function setServer(?Server $server): self
+	{
+		$this->server = $server;
+
+		return $this;
+	}
+
+	public function getDate(): ?\DateTimeInterface
+	{
+		return $this->date;
+	}
+
+	public function setDate(\DateTimeInterface $date): self
+	{
+		$this->date = $date;
+
+		return $this;
+	}
+
+	public function getAction(): ?string
+	{
+		return $this->action;
+	}
+
+	public function setAction(string $action): self
+	{
+		$this->action = $action;
+
+		return $this;
+	}
+}
