0731c8dce0b205ae8258461e30deed24f184cb52
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-07-05 11:19:28 +0200

    Moved static statistics data to server-side

--- src/Controller/StatisticsController.php
+++ src/Controller/StatisticsController.php
@@ -22,6 +22,16 @@ class StatisticsController extends AbstractController
 		}
 
 		// On affiche enfin la page des statistiques.
-		return $this->render("statistics.html.twig");
+		return $this->render("statistics.html.twig", [
+
+			// Données du nombre de joueurs connectés.
+			"player_count_data" => [ 12, 4, 1, 0, 0, 0, 1, 3, 5, 1, 2, 4, 5, 6, 4, 15, 16, 18, 23, 45, 24, 26, 45, 34, 24 ],
+
+			// Données d'utilisation du processeur du serveur (en %).
+			"cpu_usage_data" => [ 12.4, 12.68, 25, 35, 24, 12, 10, 8, 6, 1, 36, 45, 75, 89, 100, 98, 64, 54, 62, 56, 68, 72, 41, 35, 43 ],
+
+			// Données du taux de rafraîchissement du serveur (tickrate).
+			"tick_rate_data" => [ 25, 26, 27, 28, 26, 25, 24, 23, 26, 27, 30, 23, 27, 28, 35, 41, 38, 36, 33, 31, 25, 36, 26, 27, 30 ]
+		]);
 	}
 }
