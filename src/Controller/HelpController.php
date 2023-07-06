5f61ee420f092547ea20dd8f0d990aad7bae3d59
Florian Trayon <git@florian-trayon.fr>
2023-07-06 18:14:46 +0200

    Added real number of queries to support page

--- src/Controller/HelpController.php
+++ src/Controller/HelpController.php
@@ -6,6 +6,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Event;
 use App\Entity\Server;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,21 +21,20 @@ class HelpController extends AbstractController
 		// On inclut les paramètres du moteur TWIG pour la création de la page.
 		$repository = $entityManager->getRepository(User::class);
 
-		return $this->render("help.html.twig", [
+		return $this->render("help.html.twig",
+			[
+				// Nombre d'utilisateurs donateurs.
+				"help_donators_count" => $repository->count(["roles" => "ROLE_DONOR"]),
 
-			// Nombre d'utilisateurs donateurs.
-			"help_donators_count" => $repository->count(["roles" => "ROLE_DONOR"]),
+				// Nombre de serveurs enregistrés.
+				"help_servers_count" => $entityManager->getRepository(Server::class)->count([]),
 
-			// Nombre de serveurs enregistrés.
-			"help_servers_count" => $entityManager->getRepository(Server::class)->count([]),
+				// Nombre de comptes utilisateurs.
+				"help_users_count" => $repository->count([]),
 
-			// Nombre de comptes utilisateurs.
-			"help_users_count" => $repository->count([]),
-
-			// Nombre de requêtes réalisées.
-			// TODO : remplacer par le nombre de requêtes effectuées.
-			"help_requests_count" => 0
-
-		]);
+				// Nombre de requêtes réalisées.
+				"help_requests_count" => $entityManager->getRepository(Event::class)->count([]),
+			]
+		);
 	}
 }
