b0443732b0c3beab80cd00c6eea0dd44a93c9313
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-07-08 18:59:59 +0200

    Added password hash on creation or update using EasyAdmin

---
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -0,0 +1,51 @@
+<?php
+
+//
+// Écouteur d'événements pour l'administration de EasyAdmin.
+//
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class EasyAdminSubscriber implements EventSubscriberInterface
+{
+	//
+	// Initialisation de certaines dépendances de l'écouteur.
+	//
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
+
+	//
+	// Hashage du mot de passe de l'utilisateur avant sa persistance
+	//  ou son actualisation.
+	//
+    public function hashUserPassword(BeforeEntityUpdatedEvent|BeforeEntityPersistedEvent $event)
+    {
+		// On vérifie que l'entité concernée est bien de classe
+		//  des utilisateurs.
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof User))
+		{
+            return;
+        }
+
+		// Dans ce cas, on hash enfin le mot de passe de l'utilisateur
+		//  comme lors d'une inscription ou d'une modification.
+		$entity->setPassword($this->hasher->hashPassword($entity, $entity->getPassword()));
+    }
+
+	//
+	// Déclaration des écouteurs d'événements.
+	//
+	public static function getSubscribedEvents()
+	{
+		return [
+			BeforeEntityUpdatedEvent::class => ["hashUserPassword"],
+			BeforeEntityPersistedEvent::class => ["hashUserPassword"],
+		];
+	}
+}
