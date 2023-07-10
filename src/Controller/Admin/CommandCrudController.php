5883dadbd4f587ff114084546f297b9efd26d44a
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-07-10 12:47:45 +0200

    Added custom command administration in EasyAdmin

---
+++ src/Controller/Admin/CommandCrudController.php
@@ -0,0 +1,29 @@
+<?php
+
+//
+// Contrôleur des opérations CRUD des commandes personnalisées.
+//
+namespace App\Controller\Admin;
+
+use App\Entity\Command;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class CommandCrudController extends AbstractCrudController
+{
+	public static function getEntityFqcn(): string
+	{
+		return Command::class;
+	}
+
+	public function configureFields(string $pageName): iterable
+	{
+		// Ajout de la relation avec l'utilisateur concerné dans le
+		//  formulaire de création et de modification des commandes.
+		$fields = parent::configureFields($pageName);
+
+		array_splice($fields, 1, 0, [AssociationField::new("user")]);
+
+		return $fields;
+	}
+}
