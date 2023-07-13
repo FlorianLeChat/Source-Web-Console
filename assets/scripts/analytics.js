ed4d1600fc77a11d41f92ecf2d9c5ea0768a3cb5
Florian Trayon <git@florian-trayon.fr>
2023-07-13 21:52:45 +0200

    Added reCAPTCHA check request for statistics on user consent

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -2,11 +2,13 @@
 // Permet d'ajouter le mécanisme de fonctionnement de Google Analytics.
 //  Source : https://analytics.google.com/analytics/web/
 //
+const head = $( "head" );
+
 export function sendAnalytics()
 {
 	// On ajoute d'abord le script de Google Analytics.
 	const url = `https://www.googletagmanager.com/gtag/js?id=${ window.analytics_identifier }`;
-	$( "head" ).append( `<script src="${ url }" async></script>` );
+	head.append( `<script src="${ url }" async></script>` );
 
 	// On ajoute enfin le script de configuration de Google Analytics.
 	window.dataLayer = window.dataLayer || [];
@@ -26,7 +28,18 @@ export function sendAnalytics()
 //
 export function setupRecaptcha()
 {
-	// On installe le script de Google reCAPTCHA.
+	// On installe d'abord le script de Google reCAPTCHA.
 	const url = `https://www.google.com/recaptcha/api.js?render=${ window.captcha_public_key }`;
-	$( "head" ).append( `<script src="${ url }" async></script>` );
+	head.append( `<script src="${ url }" async></script>` );
+
+	// On envoie enfin une requête de vérification afin de mesurer
+	//  le trafic du site par la console de Google reCAPTCHA.
+	const timer = setInterval( () =>
+	{
+		if ( typeof window.grecaptcha !== "undefined" )
+		{
+			fetch( "", { method: "POST" } );
+			clearInterval( timer );
+		}
+	}, 1000 );
 }
