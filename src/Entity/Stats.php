3efbf472d96439bd790a05189f426abfa1913993
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-07-15 13:27:08 +0200

    Added common read-only API routes to all Doctrine entities

--- src/Entity/Stats.php
+++ src/Entity/Stats.php
@@ -5,32 +5,49 @@
 //
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\Get;
 use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
 use App\Repository\StatsRepository;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: StatsRepository::class)]
+#[ApiResource(
+	security: "is_granted(\"ROLE_ADMIN\")",
+	operations: [
+		new Get(normalizationContext: ["groups" => "stat"]),
+		new GetCollection(normalizationContext: ["groups" => "stats"])
+	]
+)]
 class Stats
 {
 	#[ORM\Id]
 	#[ORM\Column]
 	#[ORM\GeneratedValue]
+	#[Groups(["stat", "stats"])]
 	private ?int $id = null;
 
 	#[ORM\ManyToOne(inversedBy: "stats")]
 	#[ORM\JoinColumn(nullable: false)]
+	#[Groups(["stat", "stats"])]
 	private ?Server $server = null;
 
 	#[ORM\Column(type: Types::DATETIME_MUTABLE)]
+	#[Groups(["stat", "stats"])]
 	private ?\DateTimeInterface $date = null;
 
 	#[ORM\Column(type: Types::SMALLINT)]
+	#[Groups(["stat", "stats"])]
 	private ?int $playerCount = null;
 
 	#[ORM\Column(type: Types::FLOAT)]
+	#[Groups(["stat", "stats"])]
 	private ?int $cpuUsage = null;
 
 	#[ORM\Column(type: Types::FLOAT)]
+	#[Groups(["stat", "stats"])]
 	private ?int $tickRate = null;
 
 	public function getId(): ?int
