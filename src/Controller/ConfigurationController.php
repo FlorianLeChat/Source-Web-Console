8087d00475edd3bdc4d2e445f1a8188ae9cb8b2d
Florian Trayon <git@florian-trayon.fr>
2023-07-20 10:26:57 +0200

    Fixed storage crendentials fetching for remote servers

--- src/Controller/ConfigurationController.php
+++ src/Controller/ConfigurationController.php
@@ -1,18 +1,25 @@
 <?php
 
 //
-// Contrôleur de la page de la configuration.
+// Contrôleur de la page de configuration des informations de stockage.
 //
 namespace App\Controller;
 
+use App\Entity\Storage;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConfigurationController extends AbstractController
 {
+
+	//
+	// Route vers la page de configuration des informations de stockage.
+	//
 	#[Route("/configuration", name: "configuration_page")]
-	public function index(): Response
+	public function index(Request $request, EntityManagerInterface $entityManager): Response
 	{
 		// On vérifie d'abord que l'utilisateur est bien connecté avant d'accéder
 		//  à la page, sinon on le redirige vers la page d'accueil.
@@ -22,10 +29,13 @@ class ConfigurationController extends AbstractController
 		}
 
 		// On inclut enfin les paramètres du moteur TWIG pour la création de la page.
+		$repository = $entityManager->getRepository(Storage::class);
+
 		return $this->render("configuration.html.twig", [
 			// Identifiants du serveur de stockage.
-			// TODO : remplacer par les identifiants du serveur de stockage.
-			"configuration_credentials" => []
+			"configuration_credentials" => $repository->findBy([
+				"server" => intval($request->getSession()->get("serverId", 0))
+			])
 		]);
 	}
 }
