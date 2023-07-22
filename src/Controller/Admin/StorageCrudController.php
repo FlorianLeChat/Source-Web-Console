680f0fcd52211858ed0ffee852a98897db76cbcb
Florian Trayon <git@florian-trayon.fr>
2023-07-22 16:30:32 +0200

    Fixed servers associated with remote storage entities in EasyAdmin

--- src/Controller/Admin/StorageCrudController.php
+++ src/Controller/Admin/StorageCrudController.php
@@ -6,6 +6,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Storage;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class StorageCrudController extends AbstractCrudController
@@ -13,5 +14,16 @@ class StorageCrudController extends AbstractCrudController
 	public static function getEntityFqcn(): string
 	{
 		return Storage::class;
+	}
+
+	public function configureFields(string $pageName): iterable
+	{
+		// Ajout de la relation avec le serveur concerné dans le
+		//  formulaire de création et de modification des événements.
+		$fields = parent::configureFields($pageName);
+
+		array_splice($fields, 1, 0, [AssociationField::new("server")]);
+
+		return $fields;
 	}
 }
