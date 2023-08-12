c9faea797c05e06d43c9e33208778ab10e145f53
Florian Trayon <git@florian-trayon.fr>
2023-08-12 19:52:24 +0200

    Added an arbitrary unit test to freely test PHPUnit

---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('button', 'Register');
+    }
+}
