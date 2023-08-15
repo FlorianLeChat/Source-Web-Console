c4c2474b1ec408466ed47af43c48240444c44f6e
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-08-15 11:18:32 +0200

    Fixed generation and storage of data fixtures in database

--- src/DataFixtures/StatsFixture.php
+++ src/DataFixtures/StatsFixture.php
@@ -8,13 +8,14 @@ namespace App\DataFixtures;
 use App\Entity\Stats;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-final class StatsFixture extends Fixture
+final class StatsFixture extends Fixture implements DependentFixtureInterface
 {
 	public function load(ObjectManager $manager): void
 	{
 		// Récupération d'une référence à un serveur.
-		$server = $this->getReference("server1");
+		$server = $this->getReference("server0");
 
 		// Création de 24 statistiques (1 par heure, sur 24 heures).
 		for ($i = 0; $i < 24; $i++)
@@ -22,9 +23,9 @@ final class StatsFixture extends Fixture
 			$stats = new Stats();
 			$stats->setServer($server);
 			$stats->setDate(new \DateTime("+$i hours"));
-			$stats->setPlayerCount(mt_rand(0, 128));
-			$stats->setCpuUsage(mt_rand(0, 10000) / 100);
-			$stats->setTickRate(mt_rand(1, 24));
+			$stats->setPlayerCount(rand(0, 128));
+			$stats->setCpuUsage(rand(0, 10000) / 100);
+			$stats->setTickRate(rand(1, 2400) / 100);
 
 			$manager->persist($stats);
 		}
@@ -32,5 +33,12 @@ final class StatsFixture extends Fixture
 		// Sauvegarde des statistiques.
 		$manager->persist($stats);
 		$manager->flush();
+	}
+
+	public function getDependencies()
+	{
+		return [
+			ServerFixture::class
+		];
 	}
 }
