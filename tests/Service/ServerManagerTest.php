d7a03182939aeac2246a7aeaaa32920b56d3d8be
Florian Trayon <git@florian-trayon.fr>
2023-08-29 11:26:42 +0200

    Added PHPUnit tests for server managers using the Steam API

---
+++ tests/Service/ServerManagerTest.php
@@ -0,0 +1,94 @@
+<?php
+
+//
+// Tests du service de gestion des serveurs.
+//
+namespace App\Tests;
+
+use App\Entity\Server;
+use App\Service\ServerManager;
+use xPaw\SourceQuery\Exception\SocketException;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ServerManagerTest extends KernelTestCase
+{
+	// Gestionnaire de serveurs.
+	protected serverManager $serverManager;
+
+	//
+	// Initialisation des services requis début de chaque test
+	//  afin de garantir un environnement de test propre.
+	//
+	protected function setUp(): void
+	{
+		// Appel de la méthode parente.
+		parent::setUp();
+
+		// Création du service de gestion.
+		$this->serverManager = static::getContainer()->get(ServerManager::class);
+	}
+
+	public function testServerConnect(): void
+	{
+		// Création d'un serveur de jeu existant.
+		$server = new Server();
+		$server->setAddress("208.103.169.233");
+		$server->setPort(27015);
+
+		// Connexion au serveur et envoi d'une commande.
+		//  Note : un mot de passe administrateur est requis.
+		$this->expectException(SocketException::class);
+		$this->expectExceptionMessage("You must set a RCON password before trying to execute a RCON command.");
+
+		$this->serverManager->connect($server);
+		$this->serverManager->query->Rcon("sv_shutdown");
+	}
+
+	//
+	// Chiffrement et déchiffrement d'un mot de passe.
+	//
+	public function testPasswordEncryption(): void
+	{
+		// Comparaison avec un mot de passe identique.
+		$hash = $this->serverManager->encryptPassword("florian4016");
+
+		$this->assertTrue($this->serverManager->decryptPassword($hash) === "florian4016");
+
+		// Comparaison avec un mot de passe différent.
+		$hash = $this->serverManager->encryptPassword("florian4016");
+
+		$this->assertNotTrue($this->serverManager->decryptPassword($hash) === "florian4017");
+	}
+
+	//
+	// Récupération du nom d'un jeu à partir de son numéro
+	//  d'identification unique.
+	//
+	public function testGetNameByGameID()
+	{
+		// Récupération de certains noms de jeux existants.
+		$this->assertTrue($this->serverManager->getNameByGameID(240) === "Counter-Strike: Source");
+		$this->assertTrue($this->serverManager->getNameByGameID(730) === "Counter-Strike: Global Offensive");
+		$this->assertTrue($this->serverManager->getNameByGameID(4000) === "Garry's Mod");
+
+		// Récupération d'un nom de jeu inexistant (avec et sans valeur par défaut).
+		$this->assertTrue($this->serverManager->getNameByGameID(1) === "");
+		$this->assertTrue($this->serverManager->getNameByGameID(1, "Unknown") === "Unknown");
+	}
+
+	//
+	// Récupération du numéro d'identification unique d'un jeu
+	//  utilisé par l'adresse IP d'un serveur.
+	//
+	public function testGetGameIDByAddress()
+	{
+		// Récupération du jeu « Counter-Strike: Global Offensive ».
+		$this->assertTrue($this->serverManager->getGameIDByAddress("178.32.119.33", 27015) === 730);
+
+		// Récupération du jeu « Garry's Mod ».
+		$this->assertTrue($this->serverManager->getGameIDByAddress("208.103.169.233", 27015) === 4000);
+
+		// Récupération d'un jeu inexistant (le serveur n'existe pas).
+		$this->assertTrue($this->serverManager->getGameIDByAddress("127.0.0.1", 27015) === 0);
+	}
+}
