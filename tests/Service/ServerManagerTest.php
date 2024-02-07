bcf9582f5e990c9f350e015ff29f7eb3113b8e3c
Florian Trayon <git@florian-trayon.fr>
2024-02-07 21:44:24 +0100

    Updated IP addresses for game server testing using the Steam API

--- tests/Service/ServerManagerTest.php
+++ tests/Service/ServerManagerTest.php
@@ -83,7 +83,7 @@ class ServerManagerTest extends KernelTestCase
 	public function testGetGameIDByAddress(): void
 	{
 		// Récupération du jeu « Counter-Strike 2 ».
-		$this->assertTrue($this->serverManager->getGameIDByAddress("216.52.148.47", 27015) === 730);
+		$this->assertTrue($this->serverManager->getGameIDByAddress("92.119.148.31", 27015) === 730);
 
 		// Récupération du jeu « Garry's Mod ».
 		$this->assertTrue($this->serverManager->getGameIDByAddress("208.103.169.233", 27015) === 4000);
