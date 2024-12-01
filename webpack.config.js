0872a5901abd3f3a91c048967ce96200f6f311f7
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-12-01 11:48:14 +0100

    Updated CoreJS version used by Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ Encore
 	.configureBabelPresetEnv( ( config ) =>
 	{
 		config.useBuiltIns = "usage";
-		config.corejs = "3.35";
+		config.corejs = "3.39";
 	} )
 	.copyFiles( [
 		{ from: "./assets/favicons", to: "favicons/[path][name].[ext]" }, // https://github.com/symfony/webpack-encore/issues/796#issuecomment-653091438
