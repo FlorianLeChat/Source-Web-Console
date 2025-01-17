710502a457840822275546a914f6b8ba6b3d99d1
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-01-17 20:30:16 +0100

    Removed a bunch of long-unused Composer dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -10,7 +10,6 @@ return [
 	Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ["all" => true],
 	Symfony\Bundle\SecurityBundle\SecurityBundle::class => ["all" => true],
 	Symfony\Bundle\MonologBundle\MonologBundle::class => ["all" => true],
-	Symfony\Bundle\MakerBundle\MakerBundle::class => ["dev" => true],
 	Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ["all" => true],
 	EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ["all" => true],
 	Nelmio\CorsBundle\NelmioCorsBundle::class => ["all" => true],
