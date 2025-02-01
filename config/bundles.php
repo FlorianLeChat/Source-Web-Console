ab7cb384044c2a98f07262cd52cdbbe95119d333
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-02-01 11:39:30 +0100

    Fixed EasyAdmin administration interface since latest updates

--- config/bundles.php
+++ config/bundles.php
@@ -11,6 +11,7 @@ return [
 	Symfony\Bundle\SecurityBundle\SecurityBundle::class => ["all" => true],
 	Symfony\Bundle\MonologBundle\MonologBundle::class => ["all" => true],
 	Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ["all" => true],
+	Symfony\UX\TwigComponent\TwigComponentBundle::class => ["all" => true],
 	EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ["all" => true],
 	Nelmio\CorsBundle\NelmioCorsBundle::class => ["all" => true],
 	KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ["all" => true],
