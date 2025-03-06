3401f718db0976e1f675eeeed3d72c957284acc2
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-03-06 21:26:30 +0100

    Improved behavior of Docker images on various environments

---
+++ docker/scripts/development/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Generate random secret and passphrase for security operations
+sed -i "s#APP_SECRET=secret#APP_SECRET=$(openssl rand -base64 32)#g" .env && \
+sed -i "s#SSL_PHRASE=passphrase#SSL_PHRASE=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 30)#g" .env
+
+# Wait for external services to be ready
+/wait
+
+# Install Composer dependencies
+composer install
+
+# Create database and update schema
+/usr/local/bin/php bin/console doctrine:database:create --no-interaction --if-not-exists
+/usr/local/bin/php bin/console doctrine:schema:update --force
+
+# Start UDP server for incoming logs
+/usr/local/bin/php bin/console app:udp-server 127.0.0.1:8443 &
+
+# Run cron service in background
+supercronic /etc/crontabs/www-data &
+
+# Clear previous Symfony local server cache
+rm -rf ~/.symfony5/
+
+# Start Symfony server to listen on all interfaces
+symfony server:start --allow-all-ip
